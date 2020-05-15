f8d2837070723d57bc3a29141d308531e44f43fc
sya-ri <sya79lua@gmail.com>
2020-05-16 08:41:38 +0900

    送受信に成功

--- src/main/java/me/syari/ss/discord/api/JDABuilder.java
+++ src/main/java/me/syari/ss/discord/api/JDABuilder.java
@@ -93,79 +93,6 @@ public class JDABuilder
 
 
     @Nonnull
-    public JDABuilder setHttpClientBuilder(@Nullable OkHttpClient.Builder builder)
-    {
-        this.httpClientBuilder = builder;
-        return this;
-    }
-
-
-    @Nonnull
-    public JDABuilder setHttpClient(@Nullable OkHttpClient client)
-    {
-        this.httpClient = client;
-        return this;
-    }
-
-
-    @Nonnull
-    public JDABuilder setWebsocketFactory(@Nullable WebSocketFactory factory)
-    {
-        this.wsFactory = factory;
-        return this;
-    }
-
-
-    @Nonnull
-    public JDABuilder setRateLimitPool(@Nullable ScheduledExecutorService pool)
-    {
-        return setRateLimitPool(pool, pool == null);
-    }
-
-
-    @Nonnull
-    public JDABuilder setRateLimitPool(@Nullable ScheduledExecutorService pool, boolean automaticShutdown)
-    {
-        this.rateLimitPool = pool;
-        this.shutdownRateLimitPool = automaticShutdown;
-        return this;
-    }
-
-
-    @Nonnull
-    public JDABuilder setGatewayPool(@Nullable ScheduledExecutorService pool)
-    {
-        return setGatewayPool(pool, pool == null);
-    }
-
-
-    @Nonnull
-    public JDABuilder setGatewayPool(@Nullable ScheduledExecutorService pool, boolean automaticShutdown)
-    {
-        this.mainWsPool = pool;
-        this.shutdownMainWsPool = automaticShutdown;
-        return this;
-    }
-
-
-    @Nonnull
-    public JDABuilder setCallbackPool(@Nullable ExecutorService executor)
-    {
-        return setCallbackPool(executor, executor == null);
-    }
-
-
-    @Nonnull
-    public JDABuilder setCallbackPool(@Nullable ExecutorService executor, boolean automaticShutdown)
-    {
-        this.callbackPool = executor;
-        this.shutdownCallbackPool = automaticShutdown;
-        return this;
-    }
-
-
-
-    @Nonnull
     public JDABuilder setActivity(@Nullable Activity activity)
     {
         this.activity = activity;
@@ -190,71 +117,6 @@ public class JDABuilder
         Checks.noneNull(listeners, "listeners");
 
         Collections.addAll(this.listeners, listeners);
-        return this;
-    }
-
-
-    @Nonnull
-    public JDABuilder removeEventListeners(@Nonnull Object... listeners)
-    {
-        Checks.noneNull(listeners, "listeners");
-
-        this.listeners.removeAll(Arrays.asList(listeners));
-        return this;
-    }
-
-
-    @Nonnull
-    public JDABuilder setMaxReconnectDelay(int maxReconnectDelay)
-    {
-        Checks.check(maxReconnectDelay >= 32, "Max reconnect delay must be 32 seconds or greater. You provided %d.", maxReconnectDelay);
-
-        this.maxReconnectDelay = maxReconnectDelay;
-        return this;
-    }
-
-
-    @Nonnull
-    public JDABuilder useSharding(int shardId, int shardTotal)
-    {
-        AccountTypeException.check(accountType, AccountType.BOT);
-        Checks.notNegative(shardId, "Shard ID");
-        Checks.positive(shardTotal, "Shard Total");
-        Checks.check(shardId < shardTotal,
-                "The shard ID must be lower than the shardTotal! Shard IDs are 0-based.");
-        shardInfo = new JDA.ShardInfo(shardId, shardTotal);
-        return this;
-    }
-
-
-    @Nonnull
-    public JDABuilder setSessionController(@Nullable SessionController controller)
-    {
-        this.controller = controller;
-        return this;
-    }
-
-
-    @Nonnull
-    public JDABuilder setGuildSubscriptionsEnabled(boolean enabled)
-    {
-        return setFlag(ConfigFlag.GUILD_SUBSCRIPTIONS, enabled);
-    }
-
-
-    @Nonnull
-    public JDABuilder setLargeThreshold(int threshold)
-    {
-        this.largeThreshold = Math.max(50, Math.min(250, threshold)); // enforce 50 <= t <= 250
-        return this;
-    }
-
-
-    @Nonnull
-    public JDABuilder setMaxBufferSize(int bufferSize)
-    {
-        Checks.notNegative(bufferSize, "The buffer size");
-        this.maxBufferSize = bufferSize;
         return this;
     }
 
@@ -301,12 +163,4 @@ public class JDABuilder
         return jda;
     }
 
-    private JDABuilder setFlag(ConfigFlag flag, boolean enable)
-    {
-        if (enable)
-            this.flags.add(flag);
-        else
-            this.flags.remove(flag);
-        return this;
-    }
 }
