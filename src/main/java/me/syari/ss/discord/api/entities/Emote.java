69801169f325ad34cd68ca6dab0e3c41793ba015
sya-ri <sya79lua@gmail.com>
2020-05-16 22:51:37 +0900

    送受信に成功

--- src/main/java/me/syari/ss/discord/api/entities/Emote.java
+++ src/main/java/me/syari/ss/discord/api/entities/Emote.java
@@ -1,13 +1,6 @@
 package me.syari.ss.discord.api.entities;
 
-import me.syari.ss.discord.annotations.DeprecatedSince;
-import me.syari.ss.discord.annotations.ReplaceWith;
-import me.syari.ss.discord.api.JDA;
-import me.syari.ss.discord.api.managers.EmoteManager;
-
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.List;
 
 
 public interface Emote extends IMentionable, IFakeable {
--- src/main/java/me/syari/ss/discord/api/managers/EmoteManager.java
+++ src/main/java/me/syari/ss/discord/api/managers/EmoteManager.java
@@ -1,20 +1,12 @@
 package me.syari.ss.discord.api.managers;
 
 import me.syari.ss.discord.api.entities.Emote;
-import me.syari.ss.discord.api.entities.Guild;
-import me.syari.ss.discord.api.entities.Role;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.Set;
 
 
 public interface EmoteManager extends Manager<EmoteManager> {
-
-    long NAME = 0x1;
-
-    long ROLES = 0x2;
 
 
     @Nonnull
@@ -36,7 +28,4 @@ public interface EmoteManager extends Manager<EmoteManager> {
     EmoteManager setName(@Nonnull String name);
 
 
-    @Nonnull
-    @CheckReturnValue
-    EmoteManager setRoles(@Nullable Set<Role> roles);
 }
