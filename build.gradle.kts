8c2e0d9a40fb66c9f4f87427da0d0488f035c296
sya-ri <sya79lua@gmail.com>
2020-05-18 19:33:51 +0900

    送受信に成功

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,27 +25,23 @@ dependencies {
     /* ABI dependencies */
 
     //Code safety
-    implementation("org.jetbrains:annotations:16.0.1")
+    implementation("org.jetbrains:annotations:19.0.0")
 
     //Logger
     implementation("org.slf4j:slf4j-api:1.7.25")
 
     //Web Connection Support
     implementation("com.neovisionaries:nv-websocket-client:2.9")
-    implementation("com.squareup.okhttp3:okhttp:3.13.0")
+    implementation("com.squareup.okhttp3:okhttp:4.6.0")
 
 
     //Collections Utility
-    implementation("org.apache.commons:commons-collections4:4.1")
+    api("org.apache.commons:commons-collections4:4.4")
 
 
     /* Internal dependencies */
 
     //General Utility
     implementation("net.sf.trove4j:trove4j:3.0.3")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.10.1")
-
-    //Sets the dependencies for the examples
-    configurations.asMap["examplesCompile"] = configurations["apiElements"]
-    configurations.asMap["examplesRuntime"] = configurations["implementation"]
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.0")
 }
