16f2ce38c0e41458ae7d1dd2924faa26de5779ba
sya-ri <sya79lua@gmail.com>
2020-05-31 12:16:09 +0900

    非同期で DiscordMessageReceiveEvent が呼び出されるバグの修正

--- src/main/kotlin/me/syari/ss/discord/paper/DiscordConnector.kt
+++ src/main/kotlin/me/syari/ss/discord/paper/DiscordConnector.kt
@@ -4,6 +4,7 @@ import me.syari.ss.core.Main.Companion.console
 import me.syari.ss.core.auto.OnEnable
 import me.syari.ss.core.config.CreateConfig.config
 import me.syari.ss.core.config.dataType.ConfigDataType
+import me.syari.ss.core.scheduler.CustomScheduler.run
 import me.syari.ss.discord.Discord
 import me.syari.ss.discord.Main.Companion.discordPlugin
 
@@ -13,7 +14,9 @@ object DiscordConnector: OnEnable {
             val token = get("bot_token", ConfigDataType.STRING)
             if(token != null){
                 Discord.init(token){ event ->
-                    DiscordMessageReceiveEvent(event).callEvent()
+                    run(discordPlugin){
+                        DiscordMessageReceiveEvent(event).callEvent()
+                    }
                 }
                 Discord.awaitReady()
             }
