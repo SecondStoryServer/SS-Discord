a3c06e7e3a29b2696f6c4f4de561c99ddbe60efa
sya-ri <sya79lua@gmail.com>
2020-06-03 19:28:06 +0900

    不要なコードを削除

--- src/main/kotlin/me/syari/ss/discord/handle/GuildSetupController.kt
+++ src/main/kotlin/me/syari/ss/discord/handle/GuildSetupController.kt
@@ -1,27 +1,18 @@
 package me.syari.ss.discord.handle
 
-import me.syari.ss.discord.data.DataArray
 import me.syari.ss.discord.data.DataContainer
 import me.syari.ss.discord.requests.WebSocketClient
 
 internal object GuildSetupController {
-    private const val CHUNK_TIMEOUT = 10000
-    private const val MEMBER_CHUNK_REQUEST = 8
     private val setupNodes = mutableMapOf<Long, GuildSetupNode>()
-    private val pendingChunks = mutableMapOf<Long, Long>()
-    private val unavailableGuilds = mutableSetOf<Long>()
-    private var incompleteCount = 0
 
     private fun remove(id: Long) {
-        unavailableGuilds.remove(id)
         setupNodes.remove(id)
-        synchronized(pendingChunks) { pendingChunks.remove(id) }
     }
 
     fun ready(id: Long) {
         remove(id)
-        incompleteCount--
-        if (incompleteCount < 1 && !WebSocketClient.isReady) {
+        if (!WebSocketClient.isReady) {
             WebSocketClient.ready()
         }
     }
@@ -37,9 +28,6 @@ internal object GuildSetupController {
 
     fun clearCache() {
         setupNodes.clear()
-        unavailableGuilds.clear()
-        incompleteCount = 0
-        synchronized(pendingChunks) { pendingChunks.clear() }
     }
 
 }
